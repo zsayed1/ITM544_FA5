15e3dd3a92722eeb0050d043f84f2ef1686b202d
Polth <aclouet@hawk.iit.edu>
2014-12-10 12:50:36 -0600

    index result update, design add

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <div class="form";">
 	<p><h3>Please enter your information before uploading your file</h3></p>
 	<form enctype="multipart/form-data" action="result.php" method="POST">
-	<input name="name" type="text" placeholder="Your Name" size="40"><p>
+	<input name="login" type="text" placeholder="Your Name" size="40"><p>
 	<input name="email" type="text" placeholder="Your Email" size="40"><p>
 	<input name="cellphone" type="text" placeholder="Your Cellphone" size="20"><p>
 	<input type="hidden" name="MAX_FILE_SIZE" value="30000000" />
@@ -38,3 +38,4 @@ Arthur CLOUET - ITM 544 CLOUD
 </body>
 
 </html>
+
--- result.php
+++ result.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
-<html>
+
+<head>
+	<title>IITM 544 Cloud</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    	
+    <link href="design.css" rel="stylesheet" type="text/css" />
+	</head>
+		
 <body>
+
+<div id="page">
+
+ 	<div class="titre">
+   	ITM 544 CLOUD
+   	</div>  
+   	
 <?php
 
 session_start ();
@@ -19,8 +33,8 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 	echo "Possible file upload attack!\n";
 }
 
-echo 'Here is some more debugging info:';
-print_r($_FILES);
+//echo 'Here is some more debugging info:';
+//print_r($_FILES);
 
 print "</pre>";
 
@@ -32,7 +46,7 @@ use Aws\S3\S3Client;
 $client = S3Client::factory();
 
 $bucket = uniqid("php-arthurma4-", true);
-echo "Creating bucket named {$bucket}\n";
+//echo "Creating bucket named {$bucket}\n";
 $result = $client->createBucket(array(
     'Bucket' => $bucket
 ));
@@ -41,8 +55,8 @@ $result = $client->createBucket(array(
 $client->waitUntilBucketExists(array('Bucket' => $bucket));
 
 $key = $uploadfile;
-echo "Creating a new object with key {$key}";
-echo "<br />";
+//echo "Creating a new object with key {$key}";
+//echo "<br />";
 $result = $client->putObject(array(
 	'ACL' => 'public-read',
     'Bucket' => $bucket,
@@ -50,21 +64,36 @@ $result = $client->putObject(array(
     'SourceFile'   => $uploadfile
 ));
 
-echo $result['ObjectURL'] . "<br />";
-echo "<br />";
+//echo $result['ObjectURL'] . "<br />";
+//echo "<br />";
 
 $url = $result['ObjectURL'];
 
-echo "Your Name is {$_SESSION['login']}";
-echo "<br />";
-echo "Your Email is {$_SESSION['email']}";
-echo "<br />";
-echo "Your Cellphone is {$_SESSION['cellphone']}";
-echo "<br />";
 ?>
 
-<br>
-<img src="<?php echo $url; ?>" alt="picture">
+<div class="element2">
+<div class="form2";">
+<p><h2>
+<?php
 
+echo "Hello {$_SESSION['login']} !";
+echo "<br />";
+echo "Email : {$_SESSION['email']}";
+echo "<br />";
+echo "Cellphone : {$_SESSION['cellphone']}";
+echo "<br />";
+?>
+</p></h2>
+<br>
+
+<center><img src="<?php echo $url; ?>" alt="picture"></center>
+</div>
+<footer>
+Arthur CLOUET - ITM 544 CLOUD 
+</footer>
+
+
+</div>
 </body>
 </html>
+
